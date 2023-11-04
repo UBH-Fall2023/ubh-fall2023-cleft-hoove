ec82726c84753c572f94c0ff933810e9c8ea6056
Naveen Chedeti <{ID}+{chedetinaveen}@users.noreply.github.com>
2023-11-04 16:11:28 -0400

    use mantine nextjs template for web-app

---
+++ web-app/components/Header/index.tsx
@@ -0,0 +1,13 @@
+import { Box, Text } from '@mantine/core';
+
+const Header = () => {
+  return (
+    <Box w="100%">
+      <Box>
+        <Text>UB Vendor Food Tracking</Text>
+      </Box>
+    </Box>
+  );
+};
+
+export default Header;
